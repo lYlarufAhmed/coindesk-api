e1d8cd2ca7eaf62f2275797e78a3045230035afb
pickboy900 <4198957+lYlarufAhmed@users.noreply.github.com>
2022-04-16 14:08:00 +0600

    added form parser

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import 'dotenv/config';
+import getBitcoinInfo from "./routes/getBitcoinInfo"
+import 'body-parser'
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000
-
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+const urlencodedParser = bodyParser.urlencoded({extended: false})
 
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
+
+app.use('/getBitcoinInfo', urlencodedParser,  getBitcoinInfo)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on : ${PORT}`)
