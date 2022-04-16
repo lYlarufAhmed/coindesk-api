1e360b2fc9a0393c660cb0530f01511c054d7b67
pickboy900 <4198957+lYlarufAhmed@users.noreply.github.com>
2022-04-16 12:09:37 +0600

    added ts-node-dev for development environment

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import express from "express";
 import 'dotenv/config';
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on : ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening on : ${PORT}`)
 });
